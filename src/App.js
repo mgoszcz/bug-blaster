c6f6b5630c2012c68e510be98bb4bebf358923ca
Marcin Goszczynski <marcin.goszczynski@imgarena.com>
2024-12-06 16:59:09 +0100

    reducers in action

--- src/App.js
+++ src/App.js
@@ -1,12 +1,40 @@
 import "./styles.css";
+import { useReducer } from "react";
 import TicketForm from "./components/TicketForm";
+import ticketReducer from "./reducers/ticketReducer";
+import TicketList from "./components/TicketList";
+import { sortTickets } from "./utilities/sortingUtilities";
 
 function App() {
+  const initialState = {
+    tickets: [],
+    editingTicket: null,
+    sortPreference: "High to Low",
+  };
+
+  const [state, dispatch] = useReducer(ticketReducer, initialState);
+  const sortedTickets = sortTickets(state.tickets, state.sortPreference);
+
   return (
     <div className="App">
       <div className={"container"}>
         <h1>Bug Blaster</h1>
-        <TicketForm />
+        <TicketForm dispatch={dispatch} editingTicket={state.editingTicket} />
+        {state.tickets.length > 0 && (
+          <div className={"results"}>
+            <h2>All Tickets</h2>
+            <select
+              onChange={(e) =>
+                dispatch({ type: "SET_SORTING", payload: e.target.value })
+              }
+              value={state.sortPreference}
+            >
+              <option value={"High to Low"}>High to Low</option>
+              <option value={"Low to High"}>Low to High</option>
+            </select>
+            <TicketList tickets={sortedTickets} dispatch={dispatch} />
+          </div>
+        )}
       </div>
     </div>
   );
--- src/components/TicketForm.js
+++ src/components/TicketForm.js
@@ -1,9 +1,20 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { clear } from "@testing-library/user-event/dist/clear";
 
-export default function TicketForm() {
+export default function TicketForm({ dispatch, editingTicket }) {
   const [title, setTitle] = useState("");
   const [description, setDescription] = useState("");
   const [priority, setPriority] = useState("1");
+
+  useEffect(() => {
+    if (editingTicket) {
+      setTitle(editingTicket.title);
+      setDescription(editingTicket.description);
+      setPriority(editingTicket.priority);
+    } else {
+      clearForm();
+    }
+  }, [editingTicket]);
 
   const priorityLabels = {
     1: "Low",
@@ -20,12 +31,20 @@ export default function TicketForm() {
   const handleSubmit = (e) => {
     e.preventDefault();
     const ticketData = {
-      id: new Date().toISOString(),
+      id: editingTicket ? editingTicket.id : new Date().toISOString(),
       title,
       description,
       priority,
     };
+    dispatch({
+      type: editingTicket ? "UPDATE_TICKET" : "ADD_TICKET",
+      payload: ticketData,
+    });
     clearForm();
+  };
+
+  const handleCancelEdit = (e) => {
+    dispatch({ type: "CLEAR_EDITING_TICKET" });
   };
 
   return (
@@ -63,9 +82,18 @@ export default function TicketForm() {
           </label>
         ))}
       </fieldset>
-      <button type={"submit"} className={"button"}>
+      <button
+        type={"submit"}
+        className={"button"}
+        disabled={title.length === 0}
+      >
         Submit
       </button>
+      {editingTicket && (
+        <button className={"button"} onClick={handleCancelEdit}>
+          Cancel
+        </button>
+      )}
     </form>
   );
 }
